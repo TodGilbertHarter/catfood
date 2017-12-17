979b5de25d6a37bb68c8e304a976c0eba686c25b
tharter <tharter@whitsendsolutions.com>
2017-12-17 13:30:30 -0800

    added client project

---
+++ client/src/main/scala/com/giantelectronicbrain/catfood/client/CatFood.scala
@@ -0,0 +1,13 @@
+package com.giantelectronicbrain.catfood.client;
+
+import scala.scalajs.js;
+import scala.scalajs.js.annotation.JSExport;
+
+@JSExport
+object CatFood extends js.JSApp {
+  
+  @JSExport
+  override def main(): Unit = {
+    println("Hello World!")
+  }
+}
